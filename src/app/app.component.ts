bbf68a2a2a81ad706d100140f1b12fe10efba796
merlinpratheesh <merlinpratheesh@gmail.com>
2021-04-28 15:57:55 +0530

    styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zerodha';
+  
+  imgsrc = 'https://lh3.googleusercontent.com/a-/AOh14GjNt4hNEB71uqRWBO1t0wf2JdxZjQTA51f9lSTtqA=s96-c';
+
+  constructor(
+    public _d: DomSanitizer
+  ) { }
+
+  fileChange(e) {
+    if(e.srcElement.files[0]!=undefined){
+    const file = e.srcElement.files[0]; 
+    this.imgsrc = window.URL.createObjectURL(file); 
+    }
+  }
 }
+
