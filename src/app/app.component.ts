cacfab57ff53eeab240f35adc252063942600e07
merlinpratheesh <merlinpratheesh@gmail.com>
2021-04-29 10:38:56 +0530

    uploaddownload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,28 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'zerodha';
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
   profileUrl: Observable<string | null>;
   constructor(private storage: AngularFireStorage) {
-     const ref = this.storage.ref('/images0.27331376713415567');
-     this.profileUrl = ref.getDownloadURL();
+
+    const ref = this.storage.ref('/users');
+    this.profileUrl = ref.getDownloadURL();
+
   }
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const filePath = 'users';
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
 
-
+    // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+        finalize(() => this.downloadURL = fileRef.getDownloadURL() )
+     )
+    .subscribe()
+  }
 }
 
